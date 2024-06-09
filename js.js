f5990008ae8a7d3f4255c03c0d632128d5da7b41
JohnRayissus <johnraysustituido@gmail.com>
2024-06-09 15:16:10 -0700

    win condition should be done for the check

--- js.js
+++ js.js
@@ -60,71 +60,52 @@ function GameController(playerOneName = "P1", playerTwoName = "P2") {
 
   const getActivePlayer = () => activePlayer;
 
-  const printNewRound = () => {
-    gameBoard.printBoard();
-    gameBoard.dropToken(0, 1, players[0].token); // Test dropping a token in column 0 for player 'X'
-    gameBoard.printBoard();
-    gameBoard.dropToken(0, 2, "O"); // Test dropping a token in column 0 for player 'X'
+  const printNewRound = (winner) => {
     gameBoard.printBoard();
 
-    gameBoard.dropToken(1, 2, "O");
-    gameBoard.dropToken(1, 1, "O");
-    gameBoard.dropToken(1, 0, "O");
-    gameBoard.dropToken(2, 2, "O");
-    gameBoard.dropToken(0, 0, "O");
-    gameBoard.dropToken(2, 0, "O");
-    gameBoard.dropToken(2, 1, "O");
-
-
-    gameBoard.printBoard();
 
     console.log(`${getActivePlayer().name}'s turn.`);
 
     console.log("Checking for winner...");
-    const winner = checkWinner(gameBoard.getBoard(), getActivePlayer().token);
     console.log("Winner:", winner);
+
   };
 
   const playRound = (row, column) => {
     console.log(`Dropping ${getActivePlayer().name}'s token...`);
-    board.dropToken(row, column, getActivePlayer().token);
 
-    switchPlayerTurn();
-    printNewRound();
+    gameBoard.dropToken(row, column, getActivePlayer().token);
+
+    const winner = checkWinner(gameBoard.getBoard(), getActivePlayer().token);
+
+
+
+    console.log("something winner", winner)
+    if (winner === true) {
+      printNewRound(winner);
+    } else {
+      switchPlayerTurn();
+    }
   };
 
   const checkWinner = (board, activePlayer) => {
 
     console.log("activeplayer", activePlayer)
     for (let i = 0; i < 3; i++) {
-      boardRow = board[i][0].getValue()
+
 
       if (
         board[i][0].getValue() !== 0 &&
         board[i][0].getValue() === board[i][1].getValue() &&
-        board[i][0].getValue() === board[i][2].getValue()
+        board[i][0].getValue() === board[i][2].getValue() ||
+        board[0][i].getValue() !== 0 &&
+        board[1][i].getValue() &&
+        board[2][i].getValue()
       ) {
-        console.log(activePlayer, "We got a winner!");
-        // return "true";
-        // remove this for now so it doesent terminate the for loop early
+        return true
       }
     }
 
-    for (let i = 0; i < 3; i++) {
-      console.log(board[i][0].getValue(), "bad romance");
-
-      if (
-        board[i][0].getValue() !== 0 ||
-        board[i][1].getValue() !== 0 ||
-        board[i][2].getValue() !== 0
-      ) {
-        console.log(activePlayer, "We got a winner Vertically!");
-        // return "true";
-        // remove this for now so it doesent terminate the for loop early
-      }
-    }
-
-    console.log(board, "the biggest boawgghhh");
     if (
       (board[0][0].getValue() != 0 &&
         board[1][1].getValue() != 0 &&
@@ -133,27 +114,26 @@ function GameController(playerOneName = "P1", playerTwoName = "P2") {
         board[1][1].getValue() != 0 &&
         board[0][2].getValue() != 0)
     ) {
-      console.log("We got a winner Diagnolly!");
+      return true
+      //diagonal check
     }
 
-
     let tieValue = 0;
-
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
 
         console.log(tieValue, "Tie value")
         if (board[i][j].getValue() != 0) {
           tieValue++
-          console.log("Tie Value", tieValue)
           if (tieValue == 9) {
-            console.log("It's a Tie ye")
+            return true
           }
 
         }
       }
     }
 
+    return false
 
 
   };
@@ -176,40 +156,46 @@ function ScreenController() {
 
   const updateScreen = () => {
     boardDiv.textContent = ""
+
+
+    const board = game.getBoard()
+    const activePlayer = game.getActivePlayer()
+
+
+    playerDiv.textContent = `${activePlayer.name} Turn`
+    board.forEach((row, rowIndex) => {
+      row.forEach((cell, index) => {
+        const ticButton = document.createElement("button"); //created this button to be appended
+        ticButton.classList.add("cell") //adds a class
+        ticButton.textContent = cell.getValue(); //gets whatever the current Value is for that button
+        ticButton.dataset.row = rowIndex;
+        ticButton.dataset.col = index;
+        boardDiv.appendChild(ticButton);
+        console.log("index", index)
+      })
+    })
   }
 
-  const board = game.getBoard()
-  const activePlayer = game.getActivePlayer()
-
-
-  playerDiv.textContent = `${activePlayer.name} Turn`
-  board.forEach(row => {
-    row.forEach((cell, index) => {
-      const ticButton = document.createElement("button");
-      ticButton.classList.add("cell")
-      ticButton.dataset.column = index
-      ticButton.dataset.row = index
-      ticButton.textContent = cell.getValue();
-      boardDiv.appendChild(ticButton);
-      console.log("index", index)
-
-    })
-  })
 
   function clickHandlerBoard(e) {
-    const selectedColumn = e.target.dataset.column;
+    const selectedColumn = e.target.dataset.col;
     const selectedRow = e.target.dataset.row
-    if (!selectedColumn || selectedRow) {
+    console.log(selectedColumn, selectedRow)
+
+    if (!selectedColumn || !selectedRow) {
+      console.log("no")
       return
     }
 
-    ;
+
 
     game.playRound(selectedRow, selectedColumn);
     updateScreen();
   }
 
   boardDiv.addEventListener("click", clickHandlerBoard)
+
+  updateScreen()
 }
 
 ScreenController();
